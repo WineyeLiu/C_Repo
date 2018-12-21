0dbf97dba4ac35e7d903e42a53ad30e6ad2947f6
wineyeliu <mistwineye@hotmail.com>
2018-12-21 12:19:23 +0800

    commit cha11

---
+++ cha11/str_convert.c
@@ -0,0 +1,16 @@
+/* str_convert.c  - try to use strtol function 
+ 2018-12-19 21:47:49*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    char number[20];
+    char * end;
+    long value;
+    puts("please enter a number(Empty line to quit)");
+    while (gets(number) && number[0] != '\0')
+    {
+        
+    }
+}
